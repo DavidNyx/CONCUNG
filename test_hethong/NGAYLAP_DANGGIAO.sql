30c4aa9028ecf62f08c8e7d8ffd468b4a4b863ae
binhhieutu <63572337+binhhieutu@users.noreply.github.com>
2022-01-02 19:27:13 +0700

    .

---
+++ test_hethong/NGAYLAP_DANGGIAO.sql
@@ -0,0 +1,17 @@
+﻿
+  use CONCUNG
+  go
+  select NGAYLAP
+  from DONHANG
+  where TINHTRANG like N'Đang giao'
+  go
+
+  use CONCUNG_INDEX_PARTITION
+  go
+  select NGAYLAP
+  from DONHANG
+  where TINHTRANG like N'Đang giao'
+  go
+
+
+ XEMNGAYLAP_DANGGIAO
